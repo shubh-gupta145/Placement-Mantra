dbebcab6bdab72f607f997c5e44cbf62cb932627
shubh-gupta145 <beastboyshubh145@gmail.com>
2026-01-25 04:20:05 +0530

    Today I made ChatBot Page

--- Frontend/src/components/Friday A.I/FridayInterFace.jsx
+++ Frontend/src/components/Friday A.I/FridayInterFace.jsx
@@ -1,8 +1,34 @@
 import styles from "./FridayInterFace.module.css";
+import { FaPlus } from "react-icons/fa";
+import { FaUpload } from "react-icons/fa";
 function FridayInterFace() {
   return (
     <div className={styles.wrapper}>
-      <h1>Friday Interface</h1>
+        <div className={styles.SettingPannel}>
+<div className={styles.NewChat}>
+    <div className={styles.NewChatIcon}>
+        <FaPlus className="text-xl" />
+    </div>
+<span className={styles.text}> New Chat</span>
+</div>
+<div className={styles.HistoryChats}>
+<span className={styles.text}>this is you History</span>
+</div>
+        </div>
+        <div className={styles.OutputPannel}>
+      <div className={styles.DisplayScreen}>
+
+      </div>
+<div className={styles.InputPannel}>
+    <div className={styles.UploadIcon}>
+<FaUpload size={20} />
+</div>
+<div className={styles.InputContainer}>
+<input type="text" placeholder="Type your message here..." className={styles.InputBox}/>
+</div>
+<button className={styles.SendButton}>Send</button>
+</div>
+        </div>
     </div>
   );
 }
